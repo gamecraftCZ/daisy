4b50d61f4b84d8697d011405aa565085c640570b
Patrik Vacal <patrik.vacal@gmail.com>
2023-03-19 19:21:11 +0100

    Loading pretrained GPT model and Freezing parameters.

---
+++ metacentrum_job.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+### Define machine requirements ###
+#PBS -N daisy-iris
+#PBS -q gpu_long
+#PBS -l select=1:ncpus=4:mem=40gb:scratch_local=100gb:ngpus=1:gpu_mem=38gb
+#PBS -l walltime=216:00:00
+#PBS -m ae  # Email notification when job aborts (a) or ends (e)
+
+HOME_DIR=/storage/plzen1/home/patrikvacal
+
+### Check for Weights and Biases API key ###
+if test -z $WANDB_API_KEY
+then
+  echo "WANDB_API_KEY not found! Exiting."
+  exit
+fi
+
+
+### Prepare RUN env ###
+module add conda-modules  # Newest conda version
+# conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
+# pip install -r requirements.txt
+conda activate $HOME_DIR/envs/conda/iris
+
+### Define cpu LIMITS for the script! ###
+export OMP_NUM_THREADS=$PBS_NUM_PPN
+export OPENBLAS_NUM_THREADS=$PBS_NUM_PPN
+export MKL_NUM_THREADS=$PBS_NUM_PPN
+export VECLIB_MAXIMUM_THREADS=$PBS_NUM_PPN
+export NUMEXPR_NUM_THREADS=$PBS_NUM_PPN
+
+### Cache dirs ###
+export TMPDIR=$SCRATCHDIR/tmp
+export WANDB_CACHE_DIR=$SCRATCHDIR/cache_wandb
+export TORCH_HOME=$SCRATCHDIR/cache_pytorch
+export TORCH_HUB=$SCRATCHDIR/cache_pytorch
+
+### RUN ###
+cp -r $HOME_DIR/jobs/iris_default $SCRATCHDIR
+cd $SCRATCHDIR/iris_default
+python src/main.py env.train.id=BreakoutNoFrameskip-v4 common.device=cuda:0 wandb.mode=online
+
+### Copy results (and some garbage) back ###
+cd $SCRATCHDIR/iris_default
+tar czvf job-$PBS_JOBID.tgz .
+cp job-$JOB_ID.tgz $HOME_DIR/job_results  || export CLEAN_SCRATCH=false
--- src/models/transformer.py
+++ src/models/transformer.py
@@ -28,6 +28,9 @@ class TransformerConfig:
     resid_pdrop: float
     attn_pdrop: float
 
+    pretrained_weights: Optional[str] = None
+    frozen: Optional[bool] = False
+
     @property
     def max_tokens(self):
         return self.tokens_per_block * self.max_blocks
@@ -41,11 +44,66 @@ class Transformer(nn.Module):
         self.blocks = nn.ModuleList([Block(config) for _ in range(config.num_layers)])
         self.ln_f = nn.LayerNorm(config.embed_dim)
 
+        if config.pretrained_weights:
+            self.load_pretrained(config.pretrained_weights)
+
+        if config.frozen:
+            # Frozen as in Pretrained Transformers as Universal Computation Engines (https://arxiv.org/abs/2103.05247)
+            # Frozen AttentionBlocks except for layer norm.
+            assert config.pretrained_weights  # Frozen should be done only on pretrained model
+            for block in self.blocks:
+                for param in [*block.attn.parameters(), *block.mlp.parameters()]:
+                    param.requires_grad = False
+
+        print("Transformer: \n", self)
+
+    def load_pretrained(self, model_type):
+        # Credits to https://github.com/karpathy/minGPT/blob/master/mingpt/model.py#L175
+
+        """
+        Initialize a pretrained GPT model by copying over the weights
+        from a huggingface/transformers checkpoint.
+        """
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+
+        # out model weights
+        sd = self.state_dict()
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # Copy while ensuring all the parameters are aligned and match in names and shapes
+        keys = [k for k in sd_hf if not k.endswith('attn.masked_bias')]  # ignore these
+
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla nn.Linear.
+        # this means that we have to transpose these weights when we import them
+        assert len(keys) == len(sd) + 3  # 3 fewer keys for head and embeddings
+        for k in keys:
+            k_original = k.replace("transformer.h.", "blocks.")  # Convert to original GPT2 pretrained names
+            k_original = k_original.replace("transformer.", "")
+            if k_original not in sd:
+                print(f"Skipping {k_original} because it's not in the model. Dont worry, if you haven't change anything, this is expected.")
+                continue
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k_original].shape
+                with torch.no_grad():
+                    sd[k_original].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k_original].shape
+                with torch.no_grad():
+                    sd[k_original].copy_(sd_hf[k])
+
     def generate_empty_keys_values(self, n: int, max_tokens: int) -> KeysValues:
         device = self.ln_f.weight.device  # Assumption that all submodules are on the same device
         return KeysValues(n, self.config.num_heads, max_tokens, self.config.embed_dim, self.config.num_layers, device)
 
     def forward(self, sequences: torch.Tensor, past_keys_values: Optional[KeysValues] = None) -> torch.Tensor:
+        # Dropout -> Attention blocks -> LayerNorm
         assert past_keys_values is None or len(past_keys_values) == len(self.blocks)
         x = self.drop(sequences)
         for i, block in enumerate(self.blocks):
@@ -58,20 +116,22 @@ class Transformer(nn.Module):
 class Block(nn.Module):
     def __init__(self, config: TransformerConfig) -> None:
         super().__init__()
-        self.ln1 = nn.LayerNorm(config.embed_dim)
-        self.ln2 = nn.LayerNorm(config.embed_dim)
+        self.ln_1 = nn.LayerNorm(config.embed_dim)
+        self.ln_2 = nn.LayerNorm(config.embed_dim)
         self.attn = SelfAttention(config)
-        self.mlp = nn.Sequential(
-            nn.Linear(config.embed_dim, 4 * config.embed_dim),
-            nn.GELU(),
-            nn.Linear(4 * config.embed_dim, config.embed_dim),
-            nn.Dropout(config.resid_pdrop),
-        )
+        self.mlp = nn.ModuleDict(dict(
+            c_fc=nn.Linear(config.embed_dim, 4 * config.embed_dim),
+            c_proj=nn.Linear(4 * config.embed_dim, config.embed_dim),
+            act=nn.GELU(),
+            dropout=nn.Dropout(config.resid_pdrop),
+        ))
+        m = self.mlp
+        self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x))))  # MLP forward
 
     def forward(self, x: torch.Tensor, past_keys_values: Optional[KeysValues] = None) -> torch.Tensor:
-        x_attn = self.attn(self.ln1(x), past_keys_values)
+        x_attn = self.attn(self.ln_1(x), past_keys_values)
         x = x + x_attn
-        x = x + self.mlp(self.ln2(x))
+        x = x + self.mlpf(self.ln_2(x))
         return x
 
 
@@ -81,15 +141,20 @@ class SelfAttention(nn.Module):
         assert config.embed_dim % config.num_heads == 0
         assert config.attention in ('causal', 'block_causal')
         self.num_heads = config.num_heads
-        self.key = nn.Linear(config.embed_dim, config.embed_dim)
-        self.query = nn.Linear(config.embed_dim, config.embed_dim)
-        self.value = nn.Linear(config.embed_dim, config.embed_dim)
-        self.attn_drop = nn.Dropout(config.attn_pdrop)
-        self.resid_drop = nn.Dropout(config.resid_pdrop)
-        self.proj = nn.Linear(config.embed_dim, config.embed_dim)
+
+        # Attention
+        self.c_attn = nn.Linear(config.embed_dim, 3 * config.embed_dim)
+        # self.key = nn.Linear(config.embed_dim, config.embed_dim)
+        # self.query = nn.Linear(config.embed_dim, config.embed_dim)
+        # self.value = nn.Linear(config.embed_dim, config.embed_dim)
+
+        self.attn_dropout = nn.Dropout(config.attn_pdrop)
+        self.resid_dropout = nn.Dropout(config.resid_pdrop)
+        self.c_proj = nn.Linear(config.embed_dim, config.embed_dim)
 
         causal_mask = torch.tril(torch.ones(config.max_tokens, config.max_tokens))
-        block_causal_mask = torch.max(causal_mask, torch.block_diag(*[torch.ones(config.tokens_per_block, config.tokens_per_block) for _ in range(config.max_blocks)]))
+        block_causal_mask = torch.max(causal_mask, torch.block_diag(
+            *[torch.ones(config.tokens_per_block, config.tokens_per_block) for _ in range(config.max_blocks)]))
         self.register_buffer('mask', causal_mask if config.attention == 'causal' else block_causal_mask)
 
     def forward(self, x: torch.Tensor, kv_cache: Optional[KVCache] = None) -> torch.Tensor:
@@ -100,9 +165,13 @@ class SelfAttention(nn.Module):
         else:
             L = 0
 
-        q = self.query(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)   # (B, nh, T, hs)
-        k = self.key(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)     # (B, nh, T, hs)
-        v = self.value(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)   # (B, nh, T, hs)
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        # q = self.query(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)   # (B, nh, T, hs)
+        # k = self.key(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)     # (B, nh, T, hs)
+        # v = self.value(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)   # (B, nh, T, hs)
 
         if kv_cache is not None:
             kv_cache.update(k, v)
